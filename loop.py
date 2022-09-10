8cfb5b28fa4221d96e4e9df959648b57540f0a79
nicolas zoppiv <nicolaszoppi1@gmail.com>
2022-09-10 20:18:00 -0300

    functions practice

--- loop.py
+++ loop.py
@@ -1,10 +1,16 @@
-foods = ['apples', 'bread', 'cheese', 'milk', 'graves']
+# foods = ['apples', 'bread', 'cheese', 'milk',"bananas", 'graves']
 
-print (foods[0])
-print (foods[1])
-print (foods[2])
-print (foods[3])
-print (foods[4])
+# for food in foods:
+#     if food == "cheese":
+#         print("you have to buy this")
+#         break
+#     print(food)
 
-for food in foods:
-    print (food)asd
+# for letter in "Hello":
+#     print(letter)
+
+count = 4
+
+while count <= 10:
+    print (count)
+    count = count + 1
